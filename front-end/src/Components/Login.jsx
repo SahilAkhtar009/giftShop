837c37a5ea8cbc1dbc306f8bdd66680dd5fa669f
SahilAkhtar009 <sahilakhtar6656@gmail.com>
2025-07-21 09:16:46 +0530

    Update Login.jsx

--- front-end/src/Components/Login.jsx
+++ front-end/src/Components/Login.jsx
@@ -16,7 +16,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:5000/api/users/login", {
+      const res = await axios.post("https://giftshop-backend-9q1n.onrender.com/api/users/login", {
         email,
         password,
       });
